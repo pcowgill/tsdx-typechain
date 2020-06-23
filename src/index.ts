47e3c9361c3f09774285827bd1e81fbc4985f3d0
Paul Cowgill <pauldavidcowgill@gmail.com>
2020-06-23 09:20:36 -0500

    Linting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { Greeter } from '../typechain/Greeter';
+// import { ethers } from "ethers"
+import Greeter from '../artifacts/Greeter.json';
+import { Greeter as GreeterType } from './types/Greeter';
+// import { GreeterFactory } from './types/GreeterFactory';
 
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
@@ -6,7 +9,19 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+// const greeterFactory = new GreeterFactory();
+
+// (async () => {
+//   const greeter = await greeterFactory.deploy("hello");
+//   return greeter;
+// })();
+// const greeterAbi = Greeter.abi;
+// const provider = ethers.getDefaultProvider();
+// const contractAddress = "0x2bD9aAa2953F988153c8629926D22A6a5F69b14E";
+
+// let greeter: GreeterType
 
 export const types = {
   Greeter,
+  GreeterType,
 };
